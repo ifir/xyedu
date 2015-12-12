305b467c008dc2575af1d86a69dc969e0a906b11
FIR <664339636@qq.com>
2015-12-12 17:11:46 +0800

    更新

--- gulpfile.js
+++ gulpfile.js
@@ -4,31 +4,48 @@ var browserSync = require('browser-sync');
 var minjs = require('gulp-uglify');
 var mincss = require('gulp-minify-css');
 var minimg = require('gulp-imagemin');
-
-
+//路径
 var scssPath = './src/assets/scss/*.scss';
 var jsPath = './src/assets/js/*.js';
 var imgPath = './src/assets/img/*.*';
 var htmlPath = './src/*.html';
-
+//编译scss文件输出css
 gulp.task('sass', function  () {
 	gulp.src(scssPath)
 	.pipe(sass())
 	.pipe(gulp.dest('./src/dist/css'))
 });
-
+//js输出
 gulp.task('js', function  () {
 	gulp.src(jsPath)
 	.pipe(gulp.dest('./src/dist/js'))
 });
-
+//图片输出
 gulp.task('img', function  () {
+	gulp.src(imgPath)
+	.pipe(gulp.dest('./src/dist/img'))
+});
+//压缩css
+gulp.task('min:css', function  () {
+	gulp.src(scssPath)
+	.pipe(sass())
+	.pipe(mincss())
+	.pipe(gulp.dest('./src/dist/css'))
+});
+//压缩js
+gulp.task('min:js', function  () {
+	gulp.src(jsPath)
+	.pipe(minjs())
+	.pipe(gulp.dest('./src/dist/js'))
+});
+//压缩图片
+gulp.task('min:img', function  () {
 	gulp.src(imgPath)
 	.pipe(minimg())
 	.pipe(gulp.dest('./src/dist/img'))
 });
-
-gulp.task('reload', function  () {
+//实时刷新浏览器
+/*gulp.task('reload', function  () {
 	browserSync({
 		files:'**', //监听整个项目
 		server: {
@@ -36,12 +53,15 @@ gulp.task('reload', function  () {
 		},
 		port: 3000 //端口号
 	})
-});
-
+});*/
+//监视文件变化
 gulp.task('watch',function (){
-	gulp.watch([scssPath,jsPath,imgPath,htmlPath], ['reload']);
+	//gulp.watch([htmlPath,scssPath,imgPath,imgPath], ['reload']);
 	gulp.watch([scssPath], ['sass']);
 	gulp.watch([imgPath], ['img']);
+	gulp.watch([imgPath], ['js']);
 });
 
-gulp.task('default', ['sass', 'js', 'img', 'watch', 'reload']);
+gulp.task('default', ['sass', 'js', 'img', 'watch']);
+//gulp.task('default', ['sass', 'js', 'img', 'watch', 'reload']);
+gulp.task('build', ['min:css', 'min:js', 'min:img', 'watch', 'reload']);
